2ab47ea426c9d60a9634b3a1421d3c4301e04422
MahdadiChaima <68127395+MahdadiChaima@users.noreply.github.com>
2021-08-05 20:02:36 +0100

    haell

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\tasks;
+use App\Models\User;
+use Response;
+class TasksController extends Controller
+{
+    public function store(Request $request,$id)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'descrp' => 'required',
+            'time'=> 'required',
+            'endtask'=> 'required',
+            'starttask'=> 'required'
+        ]);
+        $data = new tasks();
+        $data->name = $request->name;$data->descrp= $request->descrp;$data->time = $request->time;
+        $data->starttask= $request->starttask;$data->endtask = $request->endtask;
+        if(Auth::check()){
+            $user = User::find(Auth::user()->id);
+
+            $user->tasksuser()->save($data);
+            return response()->json("Success");
+         }else{
+           return response()->json("failure");
+         }
+      
+    }
+ 
+    public function updateData(Request $request, $id)
+    { $data = tasks::find($id);
+        
+        if(!$data){
+            return   Response::json(['dataUpdated' => null , 'isUpdated' => false]);
+            }
+            
+            $updated = $data->fill($request->all())->save();
+            return  Response::json(['dataUpdated' => $data, 'isUpdated' => true]);
+        }
+    
+    public function getAllData(){
+        $data=tasks::all();
+        if(!$data){
+            return   Response::json(['data' => null , 'isEmpty' => true]);
+            }
+            else{
+                return  Response::json(['data' => $data , 'isEmpty' => false]);
+            } }
+    public function destroy($id)
+    { $data = tasks::find($id);
+            if(!$data){
+                return   Response::json(['dataDeleted' => null , 'isDeleted' => false]);
+                }
+                else{$data->delete();
+                    return  Response::json(['dataDeleted' => $data , 'isDeleted' => true]);
+                }
+          
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('/login', 'App\Http\Controllers\AuthController@login');
+Route::post('register', 'App\Http\Controllers\AuthController@register');
+Route::post('/logout', 'App\Http\Controllers\AuthController@logout');
+Route::post('/storeTask/{id}', 'App\Http\Controllers\tasksController@store');
+Route::get('/getAllTasks', 'App\Http\Controllers\tasksController@getAllData');
+Route::put('/updateTask/{id}', 'App\Http\Controllers\tasksController@updateData');
+Route::delete('/delete/{id}', 'App\Http\Controllers\tasksController@destroy');
